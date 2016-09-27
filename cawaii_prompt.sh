30d963af5dd7fb2e3b99aa30e513bf863b290589
ken1flan <ken1flan@gmail.com>
2016-09-28 07:41:11 +0900

    Add last command status

--- cawaii_prompt.sh
+++ cawaii_prompt.sh
@@ -13,6 +13,7 @@ CAWAII_PROMPT_STATUS_NG="${CAWAII_PROMPT_STATUS_NG_COLOR}く(\"\"0\"\")＞なん
 CAWAII_PROMPT_STATUS_BUG="${CAWAII_PROMPT_STATUS_BUG_COLOR}m(_ _)m promptのバグです${CAWAII_PROMPT_TURN_OFF_COLOR}"
 
 function cawaii_prompt_preprocess () {
+  local last_command_status=$?
   if [ -z $(git rev-parse --git-dir 2> /dev/null) ]; then
     if [ -n "$CAWAII_PROMPT_OLD_PS1" ]; then
       export PS1=$CAWAII_PROMPT_OLD_PS1
@@ -21,8 +22,19 @@ function cawaii_prompt_preprocess () {
     if [ -z "$CAWAII_PROMPT_OLD_PS1" ]; then
       export CAWAII_PROMPT_OLD_PS1=$PS1
     fi
-    export PS1="${CAWAII_PROMPT_DIR_COLOR}\w${CAWAII_PROMPT_TURN_OFF_COLOR}[${CAWAII_PROMPT_BRANCH_COLOR}$(git rev-parse --abbrev-ref HEAD)${CAWAII_PROMPT_TURN_OFF_COLOR}$(git_status_string)]\n\D{%H:%M} $ "
+    export PS1="$(last_command_status_string $last_command_status)${CAWAII_PROMPT_DIR_COLOR}\w${CAWAII_PROMPT_TURN_OFF_COLOR}[${CAWAII_PROMPT_BRANCH_COLOR}$(git rev-parse --abbrev-ref HEAD)${CAWAII_PROMPT_TURN_OFF_COLOR}$(git_status_string)]\n\D{%H:%M} $ "
   fi
+}
+
+function last_command_status_string () {
+  local status=$1
+  local status_string="$status"
+  if [ $status = 0 ]; then
+    status_string=""
+  else
+    status_string=$CAWAII_PROMPT_STATUS_NG
+  fi
+  echo $status_string
 }
 
 function git_status_string () {
